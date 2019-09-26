02c61e5b546bb566c664e207be89982a1ac00793
Joshen Lim <joshenlim@Joshens-MacBook-Pro.local>
2019-09-26 23:39:17 +0800

    Add schema and database integration with mysql

--- README.md
+++ README.md
@@ -4,6 +4,17 @@ Backend API server for [ase-attendance-system](https://github.com/joshenlim/ase-
 
 ## Project setup
 
+Create a .env file with the following template if it's not already created:
+```
+export DATABASE_HOST=
+export DATABASE_PORT=
+export DATABASE_USERNAME=
+export DATABASE_PASSWORD=
+export DATABASE_NAME=
+```
+
 Install required modules: `npm install`
+
+Export environment variables: `source .env`
 
 Start development server: `npm start`
--- routes/courses.js
+++ routes/courses.js
@@ -1,15 +1,32 @@
+const pool = require('../database')
 const express = require('express')
 const router = express.Router()
 
-// Mock data import
-const courses = require('../data/courses')
-
 router.get('/', function (req, res) {
   const courseCode = req.query.search.toUpperCase()
-  const filteredCourses = courses.courses.filter(course => {
-    return course.code.indexOf(courseCode) !== -1
-  }).slice(0, 10)
-  res.json(filteredCourses)
+  pool.query(`SELECT code, name, group_name FROM courses LEFT JOIN groups ON groups.course_code = courses.code WHERE code LIKE '%${courseCode}%'`, function (err, result, fields) {
+    if (err) throw new Error(err)
+
+    let formattedCourseData = []
+    result.forEach(course_data => {
+      if (formattedCourseData.some(course => course.code == course_data.code)) {
+        formattedCourseData.filter(course => course.code == course_data.code)[0].groups.push(course_data.group_name)
+      } else {
+        formattedCourseData.push({
+          code: course_data.code,
+          name: course_data.name,
+          groups: course_data.group_name ? [course_data.group_name] : []
+        })
+      }
+    })
+
+    formattedCourseData.sort((a, b) => {
+      if (parseInt(a.code.slice(-4)) > parseInt(b.code.slice(-4))) { return 1 }
+      else { return -1 }
+    })
+    
+    res.json(formattedCourseData)
+  })
 })
 
 module.exports = router
